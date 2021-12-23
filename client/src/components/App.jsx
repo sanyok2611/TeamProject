56fc39dbd80838511d2a05b3491d23cf8ca4529b
Діана Гудько <60574540+diana-2000@users.noreply.github.com>
2021-12-23 23:30:00 +0200

    FilesComponents//Reducers//Requests+db
    Co-Authored-By: Hennadii <63108609+Hennadii580@users.noreply.github.com>
    Co-Authored-By: Alexander <63105046+sanyok2611@users.noreply.github.com>

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
 import Navbar from "./navbar/Navbar";
 import './app.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Registration from "./authorization/Registration";
 import Login from "./authorization/Login";
 import {useDispatch, useSelector} from "react-redux";
 import {auth} from "../actions/user";
+import Disk from "./disk/Disk";
 
 function App() {
     const isAuth = useSelector(state => state.user.isAuth)
@@ -21,11 +22,17 @@ function App() {
             <div className='app'>
                 <Navbar/>
                 <div className="wrap">
-                    {!isAuth &&
-                    <Routes>
-						<Route path = "/registration" element={<Registration/>}/>
-						<Route path = "/login" element={<Login/>}/>
-                    </Routes>
+                    {!isAuth ?
+                        <Routes>
+							<Route path = "/registration" element={<Registration/>}/>
+							<Route path = "/login" element={<Login/>}/>
+							<Route path="*" element={<Navigate to ="/login" />}/>
+                        </Routes>
+                        :
+                        <Routes>
+							<Route exact path = "/" element={<Disk/>}/>
+							<Route path="*" element={<Navigate to ="/" />}/>
+                        </Routes>
                     }
                 </div>
             </div>
