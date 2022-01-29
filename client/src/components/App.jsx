081ddc9f3a380598f019c43fef980fc18b79077f
Alexander <63105046+sanyok2611@users.noreply.github.com>
2022-01-29 16:27:26 +0200

    Frontend avatar
    Co-Authored-By: Hennadii <63108609+Hennadii580@users.noreply.github.com>
    Co-Authored-By: Діана Гудько <60574540+diana-2000@users.noreply.github.com>

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,13 @@
-import React, {useEffect} from 'react';
+import React, {useEffect} from "react";
 import Navbar from "./navbar/Navbar";
-import './app.css'
+import "./app.css"
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Registration from "./authorization/Registration";
 import Login from "./authorization/Login";
 import {useDispatch, useSelector} from "react-redux";
 import {auth} from "../actions/user";
 import Disk from "./disk/Disk";
+import Profile from "./profile/Profile";
 
 function App() {
     const isAuth = useSelector(state => state.user.isAuth)
@@ -15,29 +16,30 @@ function App() {
     useEffect(() => {
         dispatch(auth())
     }, [])
-
-
-    return (
-        <BrowserRouter>
-            <div className='app'>
-                <Navbar/>
-                <div className="wrap">
-                    {!isAuth ?
-                        <Routes>
+	
+	
+	return (
+		<BrowserRouter>
+			<div className="app">
+				<Navbar/>
+				<div className="wrap">
+					{!isAuth ?
+						<Routes>
 							<Route path = "/registration" element={<Registration/>}/>
 							<Route path = "/login" element={<Login/>}/>
 							<Route path="*" element={<Navigate to ="/login" />}/>
-                        </Routes>
-                        :
-                        <Routes>
+						</Routes>
+						:
+						<Routes>
 							<Route exact path = "/" element={<Disk/>}/>
+							<Route exact path ="/profile" element={<Profile/>}/>
 							<Route path="*" element={<Navigate to ="/" />}/>
-                        </Routes>
-                    }
-                </div>
-            </div>
-        </BrowserRouter>
-    );
+						</Routes>
+					}
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
